962c1d9e80f994f12aafaa8f741ca66d739b869d
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-05-25 14:30:11 +0700

    Pertemuan 7: Pointer

--- Pertemuan 7/pointer.cpp
+++ Pertemuan 7/pointer.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
 
+// Fungsi penjumlahan
 void penjumlahan(int a, int b) {
     a += b;
     cout << "Hasil penjumlahan: " << a << endl;
 }
 
+//Fungsi penjumlahan dengan pointer
 void penjumlahanPointer ( int *a, int *b) {
     *a += *b;
     cout << *a << endl;
 }
 
+//Fungsi perkalian 2 berulang dengan menggunakan pointer
 void doubleValue (int *a) {
     *a *= 2;
     cout << *a << endl;
@@ -21,7 +24,7 @@ int main() {
 
     // Deklarasi pointer
     int number = 35;
-    int *pointer_number = &number;
+    int *pointer_number = &number; //Pointer untuk variabel number
 
     cout << "Nilai number: " << number << endl;
     cout << "Alamat memori variabel number: " << &number << endl;
@@ -39,11 +42,11 @@ int main() {
 
     // Pointer in array
     int num[] = {1, 2, 3, 4, 5};
-    int *pointer_num = num;
+    int *pointer_num = num; 
 
-    // cout << "Isi variabel number: " << num[0] << endl;
-    // cout << "Alamat memori variabel num : " << &num[3] << endl;
-    // cout << "Isi variabel pointer_num   : " << pointer_num << endl;
+    // cout << "Isi variabel number: " << num[0] << endl; //menampilkan isi variabel num dengan indeks 0
+    // cout << "Alamat memori variabel num : " << &num[3] << endl; //menampilkan alamat memori dari var num dengan indeks 3
+    // cout << "Isi variabel pointer_num   : " << pointer_num << endl; //menampilkan isi dari variabel pointer_num
     // cout << "Isi variabel yang ditunjuk oleh pointer_num: " << *pointer_num << endl;
     // cout << "Alamat memori variabel pointer_num: " << &pointer_num << endl;
     // cout <<endl;
@@ -62,14 +65,14 @@ int main() {
     // penjumlahan(num1, num2);
     // cout << "Nilai num1: " << num1 << endl;
     // cout << "Nilai num2: " << num2 << endl;
-    // penjumlahanPointer(&num1, &num2);
+    // penjumlahanPointer(&num1, &num2); // Jika menggunakan pointter, nilai variabel dapat diubah (return) ke nilai baru
     // cout << "Nilai num1: " << num1 << endl;
     // cout << "Nilai num2: " << num2 << endl;
 
     // Pointer in pointer
     int score = 4;
     int *pointer_score = &score;
-    int **ptr_ptr_score = &pointer_score;
+    int **ptr_ptr_score = &pointer_score; //Pointer untuk pointer_score
 
     // cout << "Isi variabel score: " << score << " dan alamat memorinya " << &score << endl;
 
@@ -83,7 +86,7 @@ int main() {
 
     // cout << "Isi variabel ptr: " << *ptr << " dan alamat variabel ptr: " << &ptr << endl;
 
-    // delete ptr; // deallocation
+    // delete ptr; // deallocation, menghapus pointer
     // cout << "Isi variabel ptr: " << *ptr << " dan alamat variabel ptr: " << &ptr << endl;
 
     // Contoh
@@ -91,7 +94,7 @@ int main() {
     // cout << "Masukkan sebuah angka = ";
     // cin >> n;
 
-    // doubleValue(&n);
+    // doubleValue(&n); // Menggunakan addres of n sebagai parameter aktual
     // cout << "Nilai angka setelah dikali 2 = " << n << endl;
 
 }
