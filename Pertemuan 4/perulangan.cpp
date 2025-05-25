8f15f611c5d513412adcf1d01abb9307f1723422
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-05-25 13:09:47 +0700

    Pertemuan 4: Perulangan

--- Pertemuan 4/perulangan.cpp
+++ Pertemuan 4/perulangan.cpp
@@ -7,7 +7,7 @@ int main()
     // Perulangan
 
 
-    // GoTo Label
+    // GoTo Label (Melompat sesuai dengan label yang dituju)
     // a:
     // cout<<"Hello World"<<endl;
     // goto b;
@@ -33,7 +33,7 @@ int main()
     //     goto genap;
     // }
 
-    // Statement while
+    // Statement while (Jalan hanya jika kondisi benar)
     // int i = 1;
     // while (i <= 10) {
     //     if (i % 2 == 0) {
@@ -41,19 +41,19 @@ int main()
     //     } i++;
     // }
 
-    // Statement do while
+    // Statement do while (Dijalankan minimal sekali, dan akan mengulang jika kondisi benar)
     // int i = 1;
     // do{
     //     cout<<i<<endl;
     // } while (i <= 0);
 
-    // Statement for
+    // Statement for (Perulangan yang dilakukan hanya jika kondisi benar, dan batas perulangan telah diketahui)
     // for (inisialisasi, kondisi, increment/decrement)
     // for (int i = 1; i <= 10; i+=2) {
     //         cout<<"Hello World"<<endl;
     // }
 
-    // Nested for
+    // Nested for (For di dalam For)
     // for (int i = 1; i <= 5; i++) {
     //     for (int j = 1; j <= 5; j++) {
     //         cout<<"* ";
@@ -61,7 +61,7 @@ int main()
     //     cout<<endl;
     // }
 
-    // Segitiga siku-siku
+    // Segitiga siku-siku (Mencetak bintang segitiga siku-siku menggunakan nested for)
     // for (int i = 1; i <= 5; i++) {
     //     for (int j = 1; j <= i; j++) {
     //         cout<<"* ";
@@ -69,7 +69,7 @@ int main()
     //     cout<<endl;
     // }
 
-    // Belah ketupat
+    // Belah ketupat (Mencetak bintang belah ketupat dengan menggunakan nested for)
     // for (int i = 1; i <= 5; i++) {
     //     for (int j = 1; j <= 5-i; j++) {
     //         cout<<" ";
