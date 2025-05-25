6a7658fcecda571bbe73732ff509ad220814f4dc
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-05-25 13:04:03 +0700

    Pertemuan 4: Perulangan

--- Pertemuan 4/latihan2.cpp
+++ Pertemuan 4/latihan2.cpp
@@ -9,6 +9,7 @@ int main()
     cout<<"Masukkan kalimat : ";
     getline(cin,kalimat);
 
+    //Untuk membuat hanya huruf vokal menjadi kapital
     // for (int i = 0; i < kalimat.length(); i++) {
     //     if (kalimat[i] == 'a' || kalimat[i] == 'i' || kalimat[i] == 'u' || kalimat[i] == 'e' || kalimat[i] == 'o') {
     //         kalimat[i] = toupper(kalimat[i]);
@@ -16,6 +17,7 @@ int main()
     // }
     // cout<<kalimat<<endl;
 
+    //Untuk membuat hanya huruf vokal saja yang kapital, dan sisanya huruf kecil
     for (int i = 0; i < kalimat.length(); i++) {
         if (kalimat[i] == 'a' || kalimat[i] == 'i' || kalimat[i] == 'u' || kalimat[i] == 'e' || kalimat[i] == 'o') {
             kalimat[i] = toupper(kalimat[i]);
