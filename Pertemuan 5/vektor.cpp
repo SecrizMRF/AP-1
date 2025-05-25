155217bd90eff34a0e32cfb32b38d23586a7d5e1
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-05-25 13:26:46 +0700

    Pertemuan 5: Array & Vector

--- Pertemuan 5/vektor.cpp
+++ Pertemuan 5/vektor.cpp
@@ -8,6 +8,7 @@ int main() {
     system("cls");
 
     //vektor
+    // Declare vector.
     vector<string> nama = {"Alya", "Rizky", "Rifki", "Fatih", "Dhafa"};
 
     // for (string karyawan : nama) {
@@ -21,12 +22,14 @@ int main() {
         cout << karyawan << endl;
     }
 
+    //Menghapus data paling belakang dari sebuah vector
     nama.pop_back();
     cout << "Setelah menghapus data: " << endl;
     for (string karyawan : nama) {
         cout << karyawan << endl;
     }
 
+    //Menghapus data vector dengan indeks +3)
     nama.erase(nama.begin() + 3);
     cout << "Setelah menghapus data: " << endl;
     for (string karyawan : nama) {
