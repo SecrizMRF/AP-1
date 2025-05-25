5b0f511b30ff9f8a976ed26ff790d2da4d55025a
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-05-25 15:22:23 +0700

    Pertemuan 8: Struct & Class

--- Pertemuan 8/accessStruct.cpp
+++ Pertemuan 8/accessStruct.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-struct Mahasiswa {
+struct Mahasiswa { //Struct pada C/C++ fungsinya mirip dengan record dalam pascal, yaitu menyimpan banyak variabel yang dengan tipe data berbeda
     string nama;
     int umur;
     float ipk;
@@ -10,16 +10,17 @@ struct Mahasiswa {
 
 int main () {
     system("cls");
-    Mahasiswa mhs1;
-    mhs1.nama = "Rizky";
-    mhs1.umur = 19;
-    mhs1.ipk = 4.00;
+    Mahasiswa mhs1; //Declare var mhs1 dengan tipe data struct
+    mhs1.nama = "Rizky"; //Mengakses struct Mahasiswa pada variabel nama
+    mhs1.umur = 19; //Mengakses struct Mahasiswa pada variabel umur
+    mhs1.ipk = 4.00; //Mengakses struct Mahasiswa pada var ipk
 
     cout << "Nama   : " << mhs1.nama << endl;
     cout << "Umur   : " << mhs1.umur << endl;
     cout << "IPK    : " << mhs1.ipk << endl;
     cout << endl;
 
+    //Mengakses struct menggunakan pointer
     Mahasiswa *ptrmhs1 = &mhs1;
     cout << "Nama   : " << ptrmhs1->nama << endl;
     cout << "Umur   : " << ptrmhs1->umur << endl;
