80a4fc9cb8ae8ef58e4a3b09f580bd9f746abad6
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-05-25 12:47:22 +0700

    Pertemuan 3: Operasi Kondisi

--- Pertemuan 3/Latihan.cpp
+++ Pertemuan 3/Latihan.cpp
@@ -11,17 +11,17 @@ int main()
     cout<<"Masukkan nilai : ";
     cin>>nilai;
 
-    if (nilai % 10 == 0)
+    if (nilai % 10 == 0) // operasi kondisi percabangan if
     {
         cout<<"Nilai anda kelipatan 10"<< endl;
     }
 
-    else if (nilai % 5 == 0)
+    else if (nilai % 5 == 0) // operasi kondisi jika tidak memenuhi kondisi if, dan dengan memberikan kondisi lainnya lagi
     {
         cout<<"Nilai anda kelipatan 5"<< endl;
     }
 
-    else
+    else //jika tidak memenuhi semua kondisi
     {
         cout<<"Nilai anda bukan kelipatan 5 atau 10"<< endl;
     }
