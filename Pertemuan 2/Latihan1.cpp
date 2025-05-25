71a5fec3a4baf11c5c571aee02b8ba8c2c026145
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-05-25 12:05:27 +0700

    Update Latihan1.cpp

--- Pertemuan 2/Latihan1.cpp
+++ Pertemuan 2/Latihan1.cpp
@@ -1,6 +1,6 @@
 // latihan luas dan volume permukaan bola
 #include <iostream>
-using namespace std;
+using namespace std; //digunakan di c++ untuk menyederhanakan syntax tanpa harus menggunakan std::
 
 int main()
 {
@@ -10,7 +10,7 @@ int main()
 
     cout << "Masukkan ruas: "; cin >> r;
 
-    luas = 4*3.14*(r*r);
+    luas = 4*3.14*(r*r); //Inisialisasi nilai var luas
     volume =(float(4)/3)*3.14*(r*r*r);
 
     cout << "Luas = " << luas << endl;
