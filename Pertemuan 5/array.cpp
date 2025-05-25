fce3f6cc09dcf9edfd1a9ea609393f86e56541b9
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-05-25 13:17:16 +0700

    Pertemuan 5: Array

--- Pertemuan 5/array.cpp
+++ Pertemuan 5/array.cpp
@@ -12,13 +12,13 @@ int main() {
     1. array kosong*/
     string nama[5];
 
-    nama[0] = "Alya";
+    nama[0] = "Alya"; //Array nama indeks 0
     nama[1] = "Rizky";
     nama[2] = "Rifki";
     nama[3] = "Fatih";
     nama[4] = "Dhafa";
 
-
+    // Inputan 5 nama
     // cout << "Masukkan 5 nama: " << endl;
     // for (int i = 0; i < 5; i++) {
     //     cout << "Nama ke-" << i + 1 << ": ";
@@ -39,7 +39,8 @@ int main() {
     //     cout << "Nama ke-" << i + 1 << ": " << nama[i] << endl;
     // }
 
-    // Multidimensional array
+    // Multidimensional array (Program array multidimensi, dengan contoh penggunaan matriks)
+    //Mencetak matriks 2x2
     // int matrix[2][3] = {{1,2,3}, {4,5,6}};
     // cout << "Matrix 2x3:" << endl;
     // for (int i = 0; i < 2; i++) {
@@ -49,6 +50,7 @@ int main() {
     //     cout << endl;
     // } 
 
+    //Mencetak matriks 3x3
     // int matrix1[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
     // cout << "Matrix 3x3:" << endl;
     // for (int i = 0; i < 3; i++) {
@@ -58,6 +60,7 @@ int main() {
     //     cout << endl;
     // }
 
+    //Mencetak matriks 4x6
     // int matrix2[4][6] = {{10,20,30,40,50,60}, {70,80,90,10,11,12}, {13,14,15,16,17,18}, {19,20,21,22,23,24}};
     // cout << "Matrix 4x6:" << endl;
     // for (int i = 0; i < 4; i++) {
@@ -72,10 +75,12 @@ int main() {
     // cout << nama1[0] << endl;
     // cout << nama1[2] << endl;
 
+    // Mencetak nama dengan tipe data array of char menggunakan for
     // for (int i = 0; i < nama1.length(); i++) {
     //     cout << nama1[i] << endl;
     // }
 
+    // Menukar value s1 dengan s2
     // string s1 = "Hello", s2 = "World";
     // s1 = s2;
     // cout << s1 << endl;
@@ -87,7 +92,7 @@ int main() {
     // cout << s1.substr(2,4) << endl;
     // cout << (s1+s2).substr(5,3) << endl;
 
-    //Operator sizeof()
+    //Operator sizeof() (untuk menampilkan ukuran byte(s) dari sebuah var)
     // short angka = 10;
     // cout << "Ukuran integer: " << sizeof(angka) << " bytes" << endl;
 
@@ -97,6 +102,7 @@ int main() {
     // string s = "Alya";
     // cout << "Ukuran string: " << sizeof(s) << " bytes" << endl;
 
+    // Menampilkan ukuran string dari array
     // string nama_karyawan[] = {"Budi", "Suep", "Aldi", "Cecep"};
     // int n = sizeof(nama_karyawan)/sizeof(string);
     // cout << n << endl;
